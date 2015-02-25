87624983dca3c142686233c7c5a350258e4f7f03
Ilya V. Matveychikov <i.matveychikov@securitycode.ru>
2015-02-25 03:36:20 +0300

    awesome wm config file

--- dotfiles-deploy.sh
+++ dotfiles-deploy.sh
@@ -2,5 +2,10 @@
 
 dotfiles=$(dirname $(readlink -f $0))
 
+# Xresources
 ln -sf $dotfiles/.Xresources $HOME
 ln -sf $dotfiles/.Xresources.* $HOME
+
+# awesome WM
+mkdir -p $HOME/.config/awesome
+ln -sf $dotfiles/.config/awesome/rc.lua $HOME/.config/awesome
