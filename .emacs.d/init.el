b228bdc8b116952c6f1a5a45c357dea9f5fe142b
Ilya Matveychikov <i.matveychikov@securitycode.ru>
2015-02-25 19:20:17 +0300

    emacs: add package management to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,37 @@
+(require 'cl)
+(require 'package)
+
+;;
+;; add package archives
+;;
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;;
+;; add required packages list
+;;
+
+(defvar required-packages
+  '( magit ) "A list of packages to ensure are installed at launch")
+
+(defun packages-installed-p ()
+  (loop for p in required-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (packages-installed-p)
+  (package-refresh-contents)
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(if (package-installed-p 'magit)
+    (global-set-key "\C-x\C-z" 'magit-status))
+
 ;;
 ;; GUI customization
 ;;
@@ -5,3 +39,20 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(global-set-key "\C-x\C-b" 'electric-buffer-list)
+
+;; C style
+
+(defun my-c-common-hook ()
+  (progn
+    (linum-mode)
+    (show-paren-mode)
+    (c-set-style "linux")))
+
+(add-hook 'c-mode-common-hook 'my-c-common-hook)
+
+;; disable backups
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
